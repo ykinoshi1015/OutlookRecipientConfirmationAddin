2dd4726701451cf3a84bddbf5cc2f1b81a72fa96
Yuna Nakaninshi <yuna.nakanishi@jp.ricoh.com>
2018-02-08 14:48:23 +0900

    powershellのファイル名変更

--- setup/sample.ps1
+++
@@ -1,4 +0,0 @@
-$ScriptPath = $(Split-Path -Parent $MyInvocation.MyCommand.Definition)
-[Reflection.Assembly]::LoadFrom( $(Join-Path $ScriptPath "DoNotDisableAddinUpdaterDll.dll.deploy"))
-$objoutlook = new-object -comobject outlook.application
-[DoNotDisableAddinUpdaterDll.DoNotDisableAddinUpdaterDllClass]::checkDisable($objoutlook.version)
--- setup/setup.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d %~dp0
-@powershell -command "Start-Process -Verb runas addinScript.bat"
